1760a6f787fe1c160b02042769ea4c29b43abdeb
Vaneath <vaneathzz@gmail.com>
2023-03-16 13:45:55 +0700

    fix tictactoe bugs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module javafx.gamehub {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens javafx.gamehub to javafx.fxml;
     exports javafx.gamehub;
+
+    opens javafx.gamehub.Tictactoe to javafx.fxml;
+    exports javafx.gamehub.Tictactoe;
 }
